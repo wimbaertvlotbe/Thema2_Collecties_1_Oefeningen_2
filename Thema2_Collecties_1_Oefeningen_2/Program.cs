70faa178de111c81bed15933135570d33dcf7c21
wimbaertvlotbe <50639680+wimbaert@users.noreply.github.com>
2022-10-11 10:49:46 +0200

    Add project files.

---
+++ Thema2_Collecties_1_Oefeningen_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Thema2_Collecties_1_Oefeningen_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lees grondig de probleemstelling en opdracht op Teams");
+        }
+    }
+}
